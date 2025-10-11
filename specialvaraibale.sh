f16e80ba0df634dd660a61fc8bcde7e2fda81e07
kavya <nallapukavya9@gmail.com>
2025-10-11 21:25:05 +0530

    specialvaraible

---
+++ specialvaraibale.sh
@@ -0,0 +1,11 @@
+special variable 
+=======
+$1, $2
+all the varaibale passed : $@
+number of varaibale: $#
+script name:$0
+present working directory:  $PWD
+home directory; $HOME
+which user running the script : $USER
+process id : $$
+last command in background : $!
