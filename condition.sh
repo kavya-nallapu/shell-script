a97279e9e54cc193464d2a3e56ea1f90423ddd6d
kavya <nallapukavya9@gmail.com>
2025-10-28 22:13:44 +0530

    install

--- condition.sh
+++ condition.sh
@@ -4,9 +4,9 @@ NUMBER=$1
 
 # -gt, -lt, -eq, -ge, -le
 
-if [$NUMBER -gt 100]
-thenecho "given number is greater then 100"
-
-else  
-   echo 'given number is less then or equal to 100"
+if [ $NUMBER -gt 100 ]
+then
+echo "given number is greater then 100"
+else 
+echo "given number is less then or equal to 100"
 fi
